fad8d754c03d048c245e3fb5c8f82dcfdf7ab988
Mehaboobkhan Patgonkar <I23255@verisk.com>
2020-06-05 15:16:58 +0530

    madhyama init

---
+++ src/main/java/com/madhyama/services/MadhyamaApplication.java
@@ -0,0 +1,13 @@
+package com.madhyama.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadhyamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadhyamaApplication.class, args);
+	}
+
+}
